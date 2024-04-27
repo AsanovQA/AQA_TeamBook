6c159df8e2b3bee1d49b61d16c8b0542dedaa0a5
ArynaBekeshka <arynabekeshko@gmail.com>
2024-04-27 12:55:52 +0200

    feat:changes page_locators and page_object and init test_negative_register_page.py

--- tests/ui_tests/data_ui_negative_registration.py
+++ tests/ui_tests/data_ui_negative_registration.py
@@ -8,14 +8,13 @@ load_dotenv()
 
 class RegisterPageData:
     REGISTER_PAGE_URL = os.getenv("REG_URL")
-    REGISTER_FIRST_NAME_VALID = fake.name()
-    REGISTER_FIRST_NAME_INVALID = fake.name(max_nb_chars=21)
-    REGISTER_LAST_NAME_VALID = fake.surname()
-    REGISTER_LAST_NAME_INVALID = fake.surname(max_nb_chars=21)
-    REGISTER_ORGANIZATION_NAME_VALID = fake.text(max_nb_chars=10)
-    REGISTER_ORGANIZATION_NAME_INVALID = fake.text(max_nb_chars=200)
-    REGISTER_BUSINESS_EMAIL_VALID = fake.email()
-    REGISTER_BUSINESS_EMAIL_INVALID = fake.email().replace('@example.com', '@invalid-gmailka.com')
-    REGISTERED_BUSINESS_EMAIL_VALID = "regiSTR23_@gmail.com"
-    REGISTER_PASSWORD_VALID = "12345678Q"
-    REGISTER_PASSWORD_INVALID = "AaAaAaAa"
+    FIRST_NAME_VALID = fake.name()
+    FIRST_NAME_INVALID = fake.name(max_nb_chars=21)
+    LAST_NAME_VALID = fake.surname()
+    LAST_NAME_INVALID = fake.surname(max_nb_chars=21)
+    ORGANIZATION_NAME_VALID = fake.text(max_nb_chars=10)
+    ORGANIZATION_NAME_INVALID = fake.text(max_nb_chars=200)
+    BUSINESS_EMAIL_VALID = fake.email()
+    BUSINESS_EMAIL_INVALID = fake.email().replace('@example.com', '@invalid-gmailka.com')
+    PASSWORD_VALID = "12345678Q"
+    PASSWORD_INVALID = "AaAaAaAa"
--- tests/ui_tests/test_negative_register_page.py
+++ tests/ui_tests/test_negative_register_page.py
@@ -8,10 +8,25 @@ from logic.pages.register_page import RegisterPage
 from tests.ui_tests.data_ui_negative_registration import RegisterPageData
 
 
+@pytest.mark.smoke
 def test_go_to_register_page(browser, register_url):
+    """ Go to registration page after opening login page """
     page = RegisterPage(browser, url=register_url)
     page.go_to_register_page()
     time.sleep(2)
     response = requests.get(os.getenv('REG_URL'))
     assert response.status_code == 200
+
+
+# def test_registration_with_invalid_email(browser,  register_url):
+#     """ Negative: Registration with invalid email """
+#     page = RegisterPage(browser, url=register_url)
+#     page.open()
+#     page.go_to_first_name()
+#     page.go_to_last_name()
+#     page.go_to_business_email()
+#     page.go_to_organization_name()
+#     page.go_to_password()
+#     page.go_to_create_org_btn()
+
 
