cbc241d1d806f62ff625873f0f1dbdaa34170b4f
Anastasiya <anastasiya.niadbailik@gmail.com>
2024-05-02 16:19:21 +0300

    fix: class WaitElement

--- logic/core/waiters.py
+++ logic/core/waiters.py
@@ -8,13 +8,16 @@ class WaitElement:
     def wait_until_element_be_visible(browser, locator):
         Wait(browser, float(os.environ["Timeout_Seconds"]), poll_frequency=1).until(
             EC.visibility_of_element_located(locator))
+        return Wait
 
     @staticmethod
     def wait_until_elements_are_visible(browser, locator):
         Wait(browser, float(os.environ["Timeout_Seconds"]), poll_frequency=1).until(
             EC.visibility_of_all_elements_located(locator))
+        return Wait
 
     @staticmethod
     def wait_alert_is_present(browser):
         Wait(browser, float(os.environ["Timeout_Seconds"]), poll_frequency=1).until(
             EC.alert_is_present())
+        return Wait
