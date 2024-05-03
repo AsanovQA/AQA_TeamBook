7291c11111f929f2f1cf3a977658db6937f9fa1c
Anastasiya <anastasiya.niadbailik@gmail.com>
2024-05-03 16:17:15 +0300

    fix: test

--- tests/apt_tests/test_projects.py
+++ tests/apt_tests/test_projects.py
@@ -21,6 +21,7 @@ def test_deactivate_project(projects):
 def test_activate_project(projects):
     """Positive: Activate a project"""
     project_id = projects.create_project()[1]
+    status = projects.deactivate_project(project_id)
     status = projects.activate_project(project_id)
     assert status == 200, f'received {status} instead of 200'
 
